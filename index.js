3cc14f45399b0d3baa576ba0f0bbb7678d63ac9f
Bperez4197 <bryceperez31@gmail.com>
2017-09-29 11:28:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+var name = "you"
+var holiday = "Independence Day"
+var days = "20"
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(string) {
+  return "Happy holidays, ${name}!"
+}
+function happyHolidayTo(string) {
+  return "Happy ${holiday}, ${name}!"
+}
+function holidayCountdown(string) {
+  return "It's ${days} days until ${holiday}!"
+}
